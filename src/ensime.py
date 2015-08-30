7e2d0384ac3c6292f556ab18855afb0b5142c2f2
yazgoo <yazgoo@gmail.com>
2015-08-30 16:20:06 +0200

    adding EnTypeCheck to neovim plugin

--- src/ensime.py
+++ src/ensime.py
@@ -8,6 +8,8 @@ class Ensime(object):
     def __init__(self, vim):
         self.browse = False
         self.vim = vim
+        self.matches = []
+        vim.command("highlight EnError ctermbg=red")
     def get_cache_port(self, where):
         f = open(".ensime_cache/" + where)
         port = f.read()
@@ -24,6 +26,8 @@ class Ensime(object):
         s.close()
     def cursor(self):
         return self.vim.current.window.cursor
+    def path(self):
+        return self.vim.current.buffer.name
     def path_start_size(self, what):
         self.vim.command("normal e")
         e = self.cursor()[1]
@@ -31,7 +35,10 @@ class Ensime(object):
         b = self.cursor()[1]
         s = e - b
         self.send('{} "{}", {}, {}, {}'.format(what,
-            self.vim.current.buffer.name, self.cursor()[0], b + 1, s))
+            self.path(), self.cursor()[0], b + 1, s))
+    @neovim.command('EnTypeCheck', range='', nargs='*', sync=True)
+    def type_check_cmd(self, args, range):
+        self.type_check("")
     @neovim.command('EnType', range='', nargs='*', sync=True)
     def type(self, args, range):
         self.path_start_size("type")
@@ -59,7 +66,15 @@ class Ensime(object):
         self.vim.command("echo '{}'".format(m))
     def handle_payload(self, payload):
         typehint = payload["typehint"]
-        if typehint == "BasicTypeInfo":
+        if typehint == "NewScalaNotesEvent":
+            notes = payload["notes"]
+            for note in notes:
+                l = note["line"]
+                c = note["col"] - 1
+                e = note["col"] + (note["end"] - note["beg"])
+                self.matches.append(self.vim.eval(
+                    "matchadd('EnError', '\\%{}l\\%>{}c\\%<{}c')".format(l, c, e)))
+        elif typehint == "BasicTypeInfo":
             self.message(payload["fullName"])
         elif typehint == "StringResponse":
             url = "http://127.0.0.1:{}/{}".format(self.get_cache_port("http"),
@@ -68,6 +83,13 @@ class Ensime(object):
                 subprocess.Popen([os.environ.get("BROWSER"), url])
                 self.browse = False
             self.message(url)
+    @neovim.autocmd('BufWritePost', pattern='*.scala', eval='expand("<afile>")',
+                    sync=True)
+    def type_check(self, filename):
+        self.send("typecheck '{}'".format(self.path()))
+        for i in self.matches:
+            self.vim.eval("matchdelete({})".format(i))
+        self.matches = []
     @neovim.autocmd('CursorMoved', pattern='*.scala', eval='expand("<afile>")',
                     sync=True)
     def unqueue(self, filename):
